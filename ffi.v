20c51b9d516dc668085675fe72f8912ea25b91ce
envsh <yatsen21@outlook.com>
2024-10-15 15:14:16 +0800

    fix/tst/rft

--- ffi.v
+++ ffi.v
@@ -1,5 +1,7 @@
 module ffi
 
+import log
+
 // error: Cannot find "libffi" pkgconfig file
 // #pkgconfig libffi
 #flag -lffi
@@ -12,52 +14,49 @@ module ffi
 
 pub const default_abi = C.FFI_DEFAULT_ABI
 
-pub const (
-	ok = C.FFI_OK
-    bad_typedef = C.FFI_BAD_TYPEDEF
-    bad_abi = C.FFI_BAD_ABI
-)
-pub const (
-    ctype_void       = C.FFI_TYPE_VOID
-    ctype_int        = C.FFI_TYPE_INT
-    ctype_float      = C.FFI_TYPE_FLOAT
-    ctype_double     = C.FFI_TYPE_DOUBLE
-    //#if 1               =
-    ctype_longdouble = C.FFI_TYPE_LONGDOUBLE
-    //#else               =
-    //FFI_TYPE_LONGDOUBLE = FFI_TYPE_DOUBLE
-    //#endif              =
-    ctype_uint8      = C.FFI_TYPE_UINT8
-    ctype_sint8      = C.FFI_TYPE_SINT8
-    ctype_uint16     = C.FFI_TYPE_UINT16
-    ctype_sint16     = C.FFI_TYPE_SINT16
-    ctype_uint32     = C.FFI_TYPE_UINT32
-    ctype_sint32     = C.FFI_TYPE_SINT32
-    ctype_uint64     = C.FFI_TYPE_UINT64
-    ctype_sint64     = C.FFI_TYPE_SINT64
-    ctype_struct     = C.FFI_TYPE_STRUCT
-    ctype_pointer    = C.FFI_TYPE_POINTER
-    ctype_complex    = C.FFI_TYPE_COMPLEX
-    // FFI_TYPE_LAST       FFI_TYPE_COMPLEX
-)
+pub const ok = C.FFI_OK
+pub const bad_typedef = C.FFI_BAD_TYPEDEF
+pub const bad_abi = C.FFI_BAD_ABI
+
+pub const ctype_void       = C.FFI_TYPE_VOID
+pub const ctype_int        = C.FFI_TYPE_INT
+pub const ctype_float      = C.FFI_TYPE_FLOAT
+pub const ctype_double     = C.FFI_TYPE_DOUBLE		//#if 1               =
+pub const ctype_longdouble = C.FFI_TYPE_LONGDOUBLE
+ //#else               =
+ //FFI_TYPE_LONGDOUBLE = FFI_TYPE_DOUBLE
+ //#endif              =
+pub const ctype_uint8      = C.FFI_TYPE_UINT8
+pub const ctype_sint8      = C.FFI_TYPE_SINT8
+pub const ctype_uint16     = C.FFI_TYPE_UINT16
+pub const ctype_sint16     = C.FFI_TYPE_SINT16
+pub const ctype_uint32     = C.FFI_TYPE_UINT32
+pub const ctype_sint32     = C.FFI_TYPE_SINT32
+pub const ctype_uint64     = C.FFI_TYPE_UINT64
+pub const ctype_sint64     = C.FFI_TYPE_SINT64
+pub const ctype_struct     = C.FFI_TYPE_STRUCT
+pub const ctype_pointer    = C.FFI_TYPE_POINTER
+pub const ctype_complex    = C.FFI_TYPE_COMPLEX
+// pub const FFI_TYPE_LAST       FFI_TYPE_COMPLEX
+
 
 // type: &int
-pub const (
-    type_void    = &C.ffi_type_void
-    type_uint8   = &C.ffi_type_uint8
-    type_sint8   = &C.ffi_type_sint8
-    type_uint16  = &C.ffi_type_uint16
-    type_sint16  = &C.ffi_type_sint16
-    type_uint32  = &C.ffi_type_uint32
-    type_sint32  = &C.ffi_type_sint32
-    type_uint64  = &C.ffi_type_uint64
-    type_sint64  = &C.ffi_type_sint64
-    type_float   = &C.ffi_type_float
-    type_double  = &C.ffi_type_double
-    type_pointer = &C.ffi_type_pointer
-	type_int = $if x64 || amd64 || arm64 { type_sint64 } $else { type_sint32}
-	type_uint = $if x64 || amd64 || arm64 { type_uint64 } $else { type_uint32}
-)
+
+pub const    type_void    = &C.ffi_type_void
+pub const    type_uint8   = &C.ffi_type_uint8
+pub const    type_sint8   = &C.ffi_type_sint8
+pub const    type_uint16  = &C.ffi_type_uint16
+pub const    type_sint16  = &C.ffi_type_sint16
+pub const    type_uint32  = &C.ffi_type_uint32
+pub const    type_sint32  = &C.ffi_type_sint32
+pub const    type_uint64  = &C.ffi_type_uint64
+pub const    type_sint64  = &C.ffi_type_sint64
+pub const    type_float   = &C.ffi_type_float
+pub const    type_double  = &C.ffi_type_double
+pub const    type_pointer = &C.ffi_type_pointer
+pub const	type_int = $if x64 || amd64 || arm64 { type_sint64 } $else { type_sint32}
+pub const	type_uint = $if x64 || amd64 || arm64 { type_uint64 } $else { type_uint32}
+
 
 @[typedef]
 struct C.ffi_type {}
@@ -96,20 +95,7 @@ pub fn get_type_obj3(ty int) voidptr {
 
 @[typedef]
 pub struct C.ffi_cif {}
-
-// pub type Cif = C.ffi_cif
-pub struct Cif {
-	a0 voidptr
-	a1 voidptr
-	a2 voidptr
-	a3 voidptr
-	a4 voidptr
-	a5 voidptr
-	a6 voidptr
-	a7 voidptr
-	a8 voidptr
-	a9 voidptr
-}
+pub type Cif = C.ffi_cif
 
 fn C.ffi_prep_cif(&Cif, voidptr, int, voidptr, voidptr) int
 
@@ -121,22 +107,18 @@ pub fn prep_cif2(cif &Cif, abi int, rtype &int) int {
     ret := C.ffi_prep_cif(cif, abi, 0, rtype, 0)
     return ret
 }
+pub fn prep_cif0(cif &Cif, rtype &int, atypes []&int) int {
+	// log.info("${@LOCATION}, ${rtype}, ${atypes.len}")
+    ret := C.ffi_prep_cif(cif, default_abi, atypes.len, rtype, atypes.data)
+    return ret
+}
 
 fn C.ffi_call(&Cif, voidptr, &u64, voidptr)
 
-pub fn call(cif &Cif, f voidptr /*fn()*/) {
-    mut rvalue := u64(0)
-    mut avalues := voidptr(0)
-    C.ffi_call(cif, f, &rvalue, avalues)
-	// printa(rvalue, tosbca(charptr(rvalue)))
-	tosbca(charptr(rvalue))
-}
-
-pub fn call2(f fn(), args ...voidptr) u64 {
-    cif := &Cif{}
-    ret := C.ffi_prep_cif(cif, 0, 0, 0, 0)
-    C.ffi_call(cif, f, 0, 0)
-    return 0
+pub fn call(cif &Cif, f voidptr /*fn()*/, rvalue voidptr, avalues []voidptr) voidptr {
+    // mut rvalue := u64(0)
+    C.ffi_call(cif, f, rvalue, avalues.data)
+	return voidptr(rvalue)
 }
 
 fn atypes2obj(atypes []int) []&Type {
@@ -148,35 +130,108 @@ fn atypes2obj(atypes []int) []&Type {
     return res
 }
 
-pub fn call3(f voidptr, atypes []int, avalues []voidptr) u64 {
-    assert atypes.len == avalues.len
-
-    argc := atypes.len
-    rtype := type_pointer
-    atypeso := atypes2obj(atypes)
-    atypesc := atypeso.data
-
-    // prepare
-    cif := Cif{}
-    rv := C.ffi_prep_cif(&cif, ffi.default_abi, argc, rtype, atypesc)
-    match rv {
-        ffi.ok {}
-        // ffi.BAD_TYPEDEF {}
-        //ffi.BAD_ABI {}
-        else{}
-    }
-    if rv == ffi.ok {
-    } else if rv == ffi.bad_typedef {
-    } else if rv == ffi.bad_abi {
-    } else {
-	}
-
-    // invoke
-    mut avalues2 := avalues.clone()
-    avaluesc := avalues2.data
-    mut rvalue := u64(0)
-    C.ffi_call(&cif, f, &rvalue, avaluesc)
-    return rvalue
+fn getarray_elemaddr<T>(val []T, n int) voidptr {
+	return voidptr(usize(val.data)+sizeof(T))
 }
 
+pub fn callfca6<T>(sym voidptr, args...Any) T {
+	mut argctys := [9]int{}
+	mut argotys := [9]&int{}
+	mut argvals := [9]voidptr{}
+
+	for i, arg in args {
+		mut fficty := 0
+		mut ffioty := unsafe{&int(vnil)}
+		mut argadr := vnil
+
+		match arg {
+			f32 { fficty = ctype_float;
+				ffioty = type_float;
+				argadr = voidptr(&arg)
+			}
+			f64 { fficty = ctype_double;
+				ffioty = type_double;
+				argadr = voidptr(&arg)
+			}
+			int { fficty = ctype_int;
+				ffioty = type_int;
+				argadr = voidptr(&arg)
+			}
+			usize { fficty = ctype_pointer;
+				ffioty = type_pointer;
+				argadr = voidptr(&arg)
+			}
+			i64 { fficty = ctype_sint64;
+				ffioty = type_sint64;
+				argadr = voidptr(&arg)
+			}
+			u64 { fficty = ctype_uint64;
+				ffioty = type_uint64;
+				argadr = voidptr(&arg)
+			}
+			u32 { fficty = ctype_sint32;
+				ffioty = type_uint32;
+				argadr = voidptr(&arg)
+			}
+			i16 { fficty = ctype_int;
+				ffioty = type_int;
+				argadr = voidptr(&arg)
+			}
+			i8 { fficty = ctype_int;
+				ffioty = type_int;
+				argadr = voidptr(&arg)
+			}
+			// C 中没有bool类型，是整数类型，所以对C函数应该可能。
+			// 但是对V的bool并不适用。需要V打开开关-d 4bytebool。
+			bool { fficty = ctype_int;
+				ffioty = type_int;
+				argadr = voidptr(&arg)
+			}
+			voidptr { fficty = ctype_pointer;
+				ffioty = type_pointer;
+				argadr = voidptr(&arg)
+				}
+			charptr {fficty = ctype_pointer;
+				ffioty = type_pointer;
+				argadr = voidptr(&arg)
+				}
+			byteptr {fficty = ctype_pointer;
+				ffioty = type_pointer;
+				argadr = voidptr(&arg)
+				}
+			string { fficty = ctype_pointer;
+				ffioty = type_pointer;
+				argadr = voidptr(&arg.str)
+				}
+			else {
+				log.warn("${@LOCATION} not support a${i} ${arg}")
+			}
+		}
+		argctys[i] = fficty
+		argotys[i] = ffioty
+		argvals[i] = argadr
+	}
+
+	///
+	retoty :=
+	match typeof[T]().idx {
+		typeof[f64]().idx {type_double}
+		typeof[f32]().idx {type_float}
+		-1 {type_pointer}
+		else{type_pointer}
+	}
+
+	cif := Cif{}
+	stv := prep_cif0(&cif, retoty, argotys[..args.len])
+	assert stv == ok
+
+	retval := Cif{}
+	assert sizeof(retval)>=16
+	rv := call(&cif, sym, &retval, argvals[..args.len])
+	// assert rv == &retval
+	if true {
+		return *(&T(rv))
+	}
+	return T{}
+}
 
