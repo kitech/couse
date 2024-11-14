d59a4f56e91e5016d3bb56741da4ee39857602bf
drswinghead <drswinghead@163.com>
2024-11-14 11:30:33 +0800

    som

--- mpv.v
+++ mpv.v
@@ -8,14 +8,15 @@ fn C.mpv_client_api_version() usize
 fn C.mpv_error_string(int) charptr
 fn C.mpv_free(voidptr)
 fn C.mpv_create() voidptr
-fn C.mpv_initialize(voidptr) int
+fn C.mpv_initialize(voidptr) cint
 fn C.mpv_destroy(voidptr)
 fn C.mpv_terminate_destroy(voidptr)
-fn C.mpv_set_option(voidptr, charptr, int, voidptr) int
-fn C.mpv_set_option_string(voidptr, charptr, voidptr) int
-fn C.mpv_command(voidptr, &charptr) int
-fn C.mpv_command_string(voidptr, charptr) int
-fn C.mpv_event_name(int) charptr
+fn C.mpv_set_option(voidptr, charptr, cint, voidptr) cint
+fn C.mpv_set_option_string(voidptr, charptr, voidptr) cint
+fn C.mpv_command(voidptr, &charptr) cint
+fn C.mpv_command_string(voidptr, charptr) cint
+fn C.mpv_command_async(voidptr, u64, &charptr) cint
+fn C.mpv_event_name(cint) charptr
 fn C.mpv_wait_event(voidptr, f64) voidptr
 fn C.mpv_wakeup(voidptr) 
 fn C.mpv_set_wakeup_callback(voidptr, voidptr, voidptr)
