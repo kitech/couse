7b042f80944e3c668bfea00f428bd78972821c74
egitop <egitop@uload.xyz>
2025-10-20 09:19:51 +0800

    upv, up deplib

--- mongoose.v
+++ mongoose.v
@@ -1,42 +1,43 @@
 module mongoose
 
 import mkuse.vpp.xnet
-import mkuse.vpp.xlog
+// import mkuse.vpp.xlog
+import log
 
-#flag @VMOD/mkuse/mongoose/mongoose.o
-#flag -I@VMOD/mkuse/mongoose/
+// #flag @VMODROOT/mongoose/mongoose.o
+#flag -I@VMODROOT/mongoose/
 #include "mongoose.h"
 #include "mongoose_typedef.h"
 
-fn C.mg_mgr_init()
-fn C.mg_mgr_free()
-fn C.mg_mgr_poll() int
-fn C.mg_bind() voidptr
-fn C.mg_set_protocol_http_websocket()
-fn C.mg_send()
-fn C.mg_send_head()
-fn C.mg_send_response_line()
-fn C.mg_http_send_error()
-fn C.mg_http_send_redirect()
-fn C.mbuf_remove()
+fn C.mg_mgr_init(...voidptr)
+fn C.mg_mgr_free(...voidptr)
+fn C.mg_mgr_poll(...voidptr) int
+fn C.mg_bind(...voidptr) voidptr
+// fn C.mg_set_protocol_http_websocket(...voidptr)
+fn C.mg_send(...voidptr) cint
+fn C.mg_send_head(...voidptr)
+fn C.mg_send_response_line(...voidptr)
+fn C.mg_http_send_error(...voidptr)
+fn C.mg_http_send_redirect(...voidptr)
+fn C.mbuf_remove(...voidptr)
 
 const (
-	EV_ACCEPT = C.MG_EV_ACCEPT
-	EV_CONNECT = C.MG_EV_CONNECT
-	EV_CLOSE = C.MG_EV_CLOSE
-	EV_TIMER = C.MG_EV_TIMER
-	EV_RECV = C.MG_EV_RECV
-	EV_SEND = C.MG_EV_SEND
-	EV_HTTP_REQUEST = C.MG_EV_HTTP_REQUEST
-	EV_HTTP_REPLY = C.MG_EV_HTTP_REPLY
+	ev_accept = C.MG_EV_ACCEPT
+	ev_connect = C.MG_EV_CONNECT
+	ev_close = C.MG_EV_CLOSE
+	// ev_timer = C.MG_EV_TIMER
+	// ev_recv = C.MG_EV_RECV
+	// ev_send = C.MG_EV_SEND
+	// ev_http_request = C.MG_EV_HTTP_REQUEST
+	// ev_http_reply = C.MG_EV_HTTP_REPLY
 )
 
-[typedef] // nothing happend
+@[typedef] // nothing happend
 struct C.mbuf {
 }
 
 // type mg_connection1 C.mg_connection
-[typedef]
+@[typedef]
 struct C.mg_connection {
 	recv_mbuf C.mbuf
 	send_mbuf C.mbuf
@@ -58,21 +59,21 @@ struct C.http_message {
 	// header_values [C.MG_MAX_HTTP_HEADERS]C.mg_str
 }
 struct C.mg_str {
-	p byteptr
+	buf byteptr
 	len C.size_t
 }
 
 pub struct Mgr {
-	cobjmem [100]byte
+	cobjmem [100]u8
 mut:
-	cobj &C.mg_mgr
+	cobj &C.mg_mgr = vnil
 }
 
 const (gmgr = &Mgr{} )
 fn init() {
 	mut mgr := gmgr
-	mgr.cobj = mgr.cobjmem as &C.mg_mgr
-	C.mg_mgr_init(mgr.cobj, 0)
+	// mgr.cobj = mgr.cobjmem as &C.mg_mgr
+	C.mg_mgr_init(mgr.cobj)
 }
 
 pub struct Conn {
@@ -80,44 +81,8 @@ mut:
 	cobj &C.mg_connection
 }
 
-pub fn (c &Conn) set_protocol_http_websocket() { C.mg_set_protocol_http_websocket(c.cobj) }
+// pub fn (c &Conn) set_protocol_http_websocket() { C.mg_set_protocol_http_websocket(c.cobj) }
 
 pub fn serve() {
 	for { C.mg_mgr_poll(gmgr.cobj, 1000) }
-}
-pub fn listen(address string) &Conn {
-	clsn := C.mg_bind(gmgr.cobj, '9999'.str, ev_handler)
-	c := &Conn{clsn}
-	c.set_protocol_http_websocket()
-	return c
-}
-
-fn ev_handler(nc *C.mg_connection, ev int, ev_data voidptr) {
-	buf := &nc.recv_mbuf
-	match ev {
-		/* Event handler code that defines behavior of the connection */
-		EV_ACCEPT {
-			xlog.info('accept $ev')
-		}
-		EV_RECV {
-			xlog.info('recv $ev')
-		}
-		EV_SEND {
-			xlog.info('send $ev')
-		}
-		EV_CLOSE {
-			xlog.info('close $ev')
-		}
-		EV_HTTP_REQUEST {
-			xlog.info('httpreq $ev')
-			hm := ev_data as &C.http_message
-			xlog.info('httpreq $ev $hm.method.p')
-		}
-		EV_HTTP_REPLY {
-			xlog.info('httprsp $ev')
-		}
-		else{
-			xlog.info('not handled $ev')
-		}
-	}
 }
